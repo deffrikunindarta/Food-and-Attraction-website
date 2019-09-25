1a01dbbde3b5acc5aceaafebc5b1336d37f6759e
gregoriusvito <vitogregorius@gmail.com>
2019-09-26 00:46:00 +0700

    vito

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,10 +1,20 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 
-class Login extends CI_Controller{
-	public function index(){
+class Login extends CI_Controller
+{
+	public function index()
+	{
 		$data['judul'] = 'Home';
 		$this->load->view('templates/header', $data);
-		$this->load->view('templates/login', $data);		
+		$this->load->view('templates/login', $data);
+	}
+
+	public function signup()
+	{
+
+		$this->load->view('templates/header', $data);
+		$this->load->view('templates/signup', $data);
 	}
 }
